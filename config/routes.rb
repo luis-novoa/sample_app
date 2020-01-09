53725faa691b524302d02a298056b03af1e7e810
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-09 12:12:46 -0400

    Sets the home and help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
