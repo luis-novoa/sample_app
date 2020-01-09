636de9d0e9f11ff83fc4c29023faf77b39ae6a58
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-09 13:11:41 -0400

    Creates about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
