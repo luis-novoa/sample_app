9f5c2ac85faa2266124b2e5572435628d945d763
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-15 15:49:49 -0400

    Adds sign up page and respective tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 
 end
