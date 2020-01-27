3de1694341f6706b663461266472e6422dcce145
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-27 19:12:57 -0400

    Adds validation for repeated emails

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,4 +50,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test 'email adresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
