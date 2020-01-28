dd932e15bfe8a431e2c6838d7a5b09f02866ff30
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-28 11:05:29 -0400

    Migrates password and adds bcrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_221652) do
+ActiveRecord::Schema.define(version: 2020_01_28_145820) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
