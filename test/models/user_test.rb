30965e03812c538f478be7aa8edd22ea8ff048ad
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-28 11:27:07 -0400

    Creates constraints for the password and tests for it

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @user = User.new(name: 'Example User', email: 'user@example.com', password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
@@ -56,5 +56,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' '*6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a'*5
+    assert_not @user.valid?
   end
 end
