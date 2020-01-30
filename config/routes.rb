ef274e1a61dc3e5c66cb412b45b79230db850e39
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-30 16:00:48 -0400

    Adds debug screen for development mode and adds users as resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users
 end
