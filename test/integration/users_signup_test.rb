07c26bf52b43b655c171e62b0d854773dc78eef7
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-31 15:30:18 -0400

    Creates tests for correct sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,4 +10,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div#error_explanation"
     assert_select "div.alert-danger"
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.alert
+  end
 end
