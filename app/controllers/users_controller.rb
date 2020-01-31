7fb8d0c70c8ad0de6071f881706520620aa99bd4
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-31 13:44:28 -0400

    Adds flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render "new"
     end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+    end
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.alert-danger"
+  end
+end
