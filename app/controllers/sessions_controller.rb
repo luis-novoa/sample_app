778643066cba2f64c88a439221283690b3252c35
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-05 13:21:23 -0400

    Add authentication to the session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+  end
 end
