a94f0fb67dc564fa532126d128b9a3da4a53bfb9
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-05 13:24:35 -0400

    Add danger flash message to the session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
+      flash[:danger] = "Invalid email/password combination"
       render "new"
     end
   end
