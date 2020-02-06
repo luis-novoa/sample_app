bfc1174cc73aa6ada2104eefb95393b033e0bf44
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-06 18:02:27 -0400

    Configure update on the user's controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def user_params
