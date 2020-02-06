70b043fede4e9f6d99947082b636548ee9d98cd3
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-06 18:57:06 -0400

    Configure successful edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render "edit"
     end
