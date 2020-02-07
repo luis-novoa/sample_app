8788a2a1b6b1a3f7d2f6e1d0d85b9a01490100f1
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-07 11:40:09 -0400

    Add user verification to edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -45,5 +46,10 @@ class UsersController < ApplicationController
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,4 +36,8 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def current_user?(user)
+    user && user == current_user
+  end
 end
