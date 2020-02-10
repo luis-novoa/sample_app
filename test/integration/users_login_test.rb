690a79e6b6dd9a3be4938f1d9b3c591847712c1d
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-10 18:20:22 -0400

    Introduce admin class of users

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -48,6 +48,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: "1")
+    assert_not_nil cookies[:remember_token]
     assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
