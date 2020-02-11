f481f40c4b13b81a591eab767fd6a38ac2f721c1
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-11 16:40:04 -0400

    Create preview for the mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
 
-    mail to: user.email, subject "Account activation"
+    mail to: user.email, subject: "Account activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
