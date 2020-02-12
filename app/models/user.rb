842f7ab76f662ed8d64bb71031448ac3d9405052
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-12 13:34:32 -0400

    Omit unauthenticated users from lists and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   def downcase_email
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
@@ -8,17 +12,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template "users/new"
     assert_select "div#error_explanation"
-    assert_select "div.alert-danger"
+    assert_select "div.field_with_errors"
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference "User.count", 1 do
       post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    # assert_template "users/show"
-    # assert_not flash.alert
-    # assert is_logged_in?
+    assert_template "users/show"
+    assert is_logged_in?
   end
 end
