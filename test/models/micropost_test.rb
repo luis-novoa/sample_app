bc7660f08f1f04d76517236e0af01afcb057078d
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-19 13:30:20 -0400

    Add associations for user and refactor test

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
