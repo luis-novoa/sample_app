eb766205ff6f646b8eba3913d31036bdfc306f82
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-20 13:10:57 -0400

    Implement image uploading

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select "div.pagination"
+    assert_select "input[type='file']"
     # Invalid submission
     assert_no_difference "Micropost.count" do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,9 +18,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", "/?page=2" # Correct pagination link
     # Valid submission
     content = "This micropost really ties the room together"
+    image = fixture_file_upload("test/fixtures/kitten.jpg", "image/jpeg")
     assert_difference "Micropost.count", 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, image: image } }
     end
+    assert assigns(:micropost).image.attached?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
