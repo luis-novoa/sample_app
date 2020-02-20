43e1d118e54c25f0336eb6dfd27f3881b1e222c0
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-20 13:39:00 -0400

    Add image validation

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',      '6.0.2.1'
+gem 'active_storage_validations', '0.8.2'
 gem 'bcrypt',     '3.1.13'
 gem 'faker',      '2.1.2'
 gem 'will_paginate', '3.1.8'
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,5 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid format" }, size: { less_than: 5.megabytes, message: "should be less than 5MB" }
 end
